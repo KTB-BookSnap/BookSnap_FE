e5d5fe348d3b4d1e90a9ddd62eccf675aa230d80
CoderDuck <hongdongk@naver.com>
2025-02-26 16:33:20 +0900

    feat: prompt 페이지 추가

--- frontapp/src/components/header.tsx
+++ frontapp/src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="w-full h-[100px] bg-[#5D4037] text-white text-[2.5rem] flex justify-start items-center px-5">
+    <div className="w-full h-[100px] bg-amber-950 text-amber-50 text-[2.5rem] flex justify-start items-center px-5">
       📚 &nbsp;BookSnap
     </div>
   );
