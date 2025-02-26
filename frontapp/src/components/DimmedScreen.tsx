12094c07865611538f9f09b3214bdf5bd1887ef3
jiyeeeah <sije3757@snu.ac.kr>
2025-02-26 16:38:34 +0900

    style: DimmedScreen 색상 수정

--- frontapp/src/components/DimmedScreen.tsx
+++ frontapp/src/components/DimmedScreen.tsx
@@ -1,5 +1,5 @@
 export default function DimmedScreen() {
   return (
-    <div className="absolute top-0 left-0 w-full h-full bg-[rgb(265,265,265)]/50" />
+    <div className="absolute top-0 left-0 w-full h-full bg-[rgb(200,200,200)]/50" />
   );
 }
