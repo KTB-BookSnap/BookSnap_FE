2f6f211a96c51b29fce4cb73e6f88cccff5d13f8
jiyeeeah <sije3757@snu.ac.kr>
2025-02-27 14:12:41 +0900

    feat: card/[id] 페이지 모달과 비슷하게 구현

--- frontapp/src/app/card/[id]/page.tsx
+++ frontapp/src/app/card/[id]/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import CloseIcon from "@/icons/close.svg";
+import Card from "@/components/Card";
+
 export default function CardPage() {
-  return <div className="text-amber-950">card page</div>;
+  return (
+    <div className="fixed inset-0 w-full h-full flex items-center justify-center z-20">
+      <div className="flex flex-col items-center gap-5 bg-amber-50 pt-5 pr-5 pb-10 pl-5 rounded-3xl z-10 shadow-lg">
+        <Image src={CloseIcon} alt="모달 닫기 버튼" width={20} height={20} className="self-end" />
+        <Card phrase="예시 텍스트입니다." />
+      </div>
+    </div>
+  );
 }
