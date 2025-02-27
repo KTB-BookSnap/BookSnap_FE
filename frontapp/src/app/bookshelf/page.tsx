a56d32ae0620dc191b9765ef0a434d33ddad140b
jiyeeeah <sije3757@snu.ac.kr>
2025-02-27 10:20:03 +0900

    style: 모달 열릴 때 레이아웃 바뀌는 오류 수정

--- frontapp/src/app/bookshelf/page.tsx
+++ frontapp/src/app/bookshelf/page.tsx
@@ -8,7 +8,7 @@ export default function BookShelf() {
       <div className="text-amber-950 text-4xl text-center">
         카드를 보고 싶은 책을 선택해주세요
       </div>
-      <div className="w-full flex flex-row flex-wrap gap-[100px] items-center justify-center p-5">
+      <div className="w-full flex flex-row flex-wrap gap-[80px] items-center justify-center p-5">
         <Card src="/images/sampleImage.jpg" title="제목" id={1} />
         <Card src="/images/sampleImage.jpg" title="제목" id={1} />
         <Card src="/images/sampleImage.jpg" title="제목" id={1} />
