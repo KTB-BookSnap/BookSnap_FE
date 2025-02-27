89d3da9ee1a4c01a879326c18ab5e46162503912
jiyeeeah <sije3757@snu.ac.kr>
2025-02-27 14:18:34 +0900

    feat: 모달 바깥쪽 클릭시 이전화면으로 돌아가도록
    - 추가로 x 버튼 클릭시 이전 화면으로 돌아가도록 구현

--- frontapp/src/app/@modal/(.)card/[id]/page.tsx
+++ frontapp/src/app/@modal/(.)card/[id]/page.tsx
@@ -5,8 +5,20 @@ import Image from "next/image";
 import CloseIcon from "@/icons/close.svg";
 import { useEffect } from "react";
 import Card from "@/components/Card";
+import { useRouter } from "next/navigation";
 
 export default function CardModalPage() {
+  const router = useRouter();
+
+  const handleDimmedClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    router.back();
+  };
+
+  const handleDialogClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
@@ -15,9 +27,22 @@ export default function CardModalPage() {
   }, []);
 
   return (
-    <div className="fixed inset-0 w-full h-full flex items-center justify-center z-20">
-      <div className="flex flex-col items-center gap-5 bg-amber-50 pt-5 pr-5 pb-10 pl-5 rounded-3xl z-10 shadow-lg">
-        <Image src={CloseIcon} alt="모달 닫기 버튼" width={20} height={20} className="self-end" />
+    <div
+      className="fixed inset-0 w-full h-full flex items-center justify-center z-20"
+      onClick={handleDimmedClick}
+    >
+      <div
+        className="flex flex-col items-center gap-5 bg-amber-50 pt-5 pr-5 pb-10 pl-5 rounded-3xl z-10 shadow-lg"
+        onClick={handleDialogClick}
+      >
+        <Image
+          src={CloseIcon}
+          alt="모달 닫기 버튼"
+          width={20}
+          height={20}
+          className="self-end"
+          onClick={() => router.back()}
+        />
         <Card phrase="예시 텍스트입니다." />
       </div>
       <DimmedScreen />
