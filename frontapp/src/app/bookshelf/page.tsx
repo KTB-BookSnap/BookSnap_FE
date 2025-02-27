017f1b8216da4116135f3af325c4e49f6d60cf8c
jiyeeeah <sije3757@snu.ac.kr>
2025-02-27 10:10:46 +0900

    chore: 메인페이지 prompt 페이지로 변경
    - 책 보관함 페이지는 bookshelf페이지로 디렉토리 명 변경

---
+++ frontapp/src/app/bookshelf/page.tsx
@@ -0,0 +1,20 @@
+import Card from "@/components/Card";
+import Header from "@/components/header";
+
+export default function BookShelf() {
+  return (
+    <div className="flex flex-col gap-[20px]">
+      <Header />
+      <div className="text-amber-950 text-4xl text-center">
+        카드를 보고 싶은 책을 선택해주세요
+      </div>
+      <div className="w-full flex flex-row flex-wrap gap-[100px] items-center justify-center p-5">
+        <Card src="/images/sampleImage.jpg" title="제목" id={1} />
+        <Card src="/images/sampleImage.jpg" title="제목" id={1} />
+        <Card src="/images/sampleImage.jpg" title="제목" id={1} />
+        <Card src="/images/sampleImage.jpg" title="제목" id={1} />
+        <Card src="/images/sampleImage.jpg" title="제목" id={1} />
+      </div>
+    </div>
+  );
+}
