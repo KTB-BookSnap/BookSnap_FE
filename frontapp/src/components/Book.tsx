3583e18c625ce27da42a5f6bfe6bfaba8e99884b
Dongkeun <hongdongk@naver.com>
2025-02-27 21:47:33 +0900

    Update Book.tsx

--- frontapp/src/components/Book.tsx
+++ frontapp/src/components/Book.tsx
@@ -1,7 +1,6 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 interface IBook {
   src: string;
@@ -9,18 +8,16 @@ interface IBook {
   id: number;
 }
 
+/* eslint-disable @next/next/no-img-element */
 export default function Book({ src, title, id }: IBook) {
   const router = useRouter();
 
   return (
-    <div
-      className="flex flex-col items-start gap-[10px] px-5 w-fit"
-      onClick={() => router.push(`/card/${id}`)}
-    >
+    <div className="flex flex-col items-start gap-[10px] px-5 w-fit" onClick={() => router.push(`/card/${id}`)}>
       <div className="relative w-[250px] h-[350px] drop-shadow-md">
-        {src && <Image src={src} alt="썸네일" fill={true} objectFit="cover" />}
+        {src && <img src={decodeURIComponent(src)} alt="S3 이미지" width={250} height={300} />}
       </div>
-      <div className="flex flex-row pl-[10px] items-start text-amber-950">{title}</div>
+      <div className="flex flex-row pl-[10px] items-start text-amber-950 mt-4">{title}</div>
     </div>
   );
 }
