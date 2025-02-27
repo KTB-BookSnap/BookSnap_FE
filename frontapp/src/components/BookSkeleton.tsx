aac1e886bc532a8db9e91fd209589fc001e6bc03
jiyeeeah <sije3757@snu.ac.kr>
2025-02-27 15:05:06 +0900

    style: animation 추가
    - Book 스켈레톤 컴포넌트에 animation 추가

--- frontapp/src/components/BookSkeleton.tsx
+++ frontapp/src/components/BookSkeleton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import loadingAnimation from "../../public/animations/bookLoadingAnimation.json";
+import Lottie from "react-lottie-player";
 
 export default function BookSkeleton() {
   const [pulse, setPulse] = useState(true);
@@ -9,7 +11,7 @@ export default function BookSkeleton() {
     // 깜빡이는 효과를 위한 인터벌 설정
     const interval = setInterval(() => {
       setPulse((prev) => !prev);
-    }, 800); // 0.8초마다 깜빡임
+    }, 1000); // 1초마다 깜빡임
 
     return () => clearInterval(interval);
   }, []);
@@ -18,12 +20,19 @@ export default function BookSkeleton() {
     <div className="flex flex-col items-start gap-[10px] px-5 w-fit">
       <div
         className={`relative w-[250px] h-[350px] drop-shadow-md ${
-          pulse ? "bg-amber-950/50" : "bg-amber-950/30"
+          pulse ? "bg-amber-950/50" : "bg-amber-950/40"
         } transition-colors duration-500`}
-      />
+      >
+        <Lottie
+          loop
+          animationData={loadingAnimation}
+          play
+          className="w-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
       <div
         className={`w-[150px] h-[20px] ${
-          pulse ? "bg-amber-950/50" : "bg-amber-950/30"
+          pulse ? "bg-amber-950/50" : "bg-amber-950/40"
         } transition-colors duration-500 rounded`}
       />
     </div>
